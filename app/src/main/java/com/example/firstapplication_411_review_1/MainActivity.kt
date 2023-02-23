f72c3d877529eeef5957cd12a055a20b99765dd7
NaeemK1188 <naeemalbirkdar@aol.com>
2023-02-23 01:59:52 -0800

    adding new UI

--- app/src/main/java/com/example/firstapplication_411_review_1/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication_411_review_1/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.firstapplication_411_review_1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
